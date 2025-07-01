d97208a852f8b313240addc37a5e8ef98d616da6
gaogao-9 <gaotuki@gmail.com>
2025-07-02 03:54:15 +0900

    fix: ogp

--- src/utils/createHeadMetadata.ts
+++ src/utils/createHeadMetadata.ts
@@ -4,12 +4,12 @@ import { Metadata } from "next";
 export const createHeadMetadata = (pathname: string): Metadata => {
   const route = routes.find((x) => x.pathname === pathname);
   const title =
-    "政剣マニフェスティアオンリー同人誌即売会 緊急交流イベント 漕ぎ出せ！ソクバイ海Ⅶ";
+    "政剣マニフェスティアオンリー同人誌即売会 緊急交流イベント 漕ぎ出せ！ソクバイ海Ⅷ";
   const subTitle = route?.pathname === "/top" ? "" : route?.title ?? "";
   const fullTitle = subTitle ? `${title} | ${subTitle}` : title;
   const description = `${title}の公式サイトです`;
   const origin = "https://festia.moe";
-  const ogpImage = `${origin}/ogp_icon.jpg?only2024`;
+  const ogpImage = `${origin}/ogp_icon.jpg?only2025`;
 
   return {
     title: fullTitle,
